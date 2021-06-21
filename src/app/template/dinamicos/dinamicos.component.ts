d81a967d407ef26835d6dda137aabc297c2cdf4c
Daniel-Cas <dmattcomics@gmail.com>
2021-06-21 14:06:06 -0500

    Formularios por template términados

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,19 +1,50 @@
 import { NgForm } from '@angular/forms';
 import { Component, OnInit, ViewChild } from '@angular/core';
 
+
+interface Persona{
+  nombre   : string;
+  favoritos: Favorito[];
+}
+
+interface Favorito{
+  id    : number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
 
-  @ViewChild('miFormulario') miFormulario!: NgForm;
+  nuevoJuego: string = '';
 
-  constructor() { }
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Daniel',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear'},
+      { id: 2, nombre: 'Death Stranding'}
+    ]
+    
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = { 
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push( { ...nuevoFavorito } );
+    this.nuevoJuego = '';
+  }
+
+
+  eliminar( index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
   guardar(){
